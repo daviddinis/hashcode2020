30e4ee218b18c71cda3c86097cea33ee88c912a0
Jose Gomes <zemiguelgomes99@gmail.com>
2020-02-20 18:05:18 +0000

    up

--- hashcode.js
+++ hashcode.js
@@ -4,16 +4,20 @@ function events(){
     console.log("edded event");
 }
 function readFileAsString() {
-    var files = this.files;
+    let files = this.files;
     if (files.length === 0) {
         console.log("No file is selected");
         return;
     }
 
-    var reader = new FileReader();
+    let reader = new FileReader();
     reader.onload = function(event) {
-        console.log("File content:", event.target.result);
+        console.log("File content:", event.target.result)
+        let text = document.getElementById("filecontent")
+        text.innerHTML = event.target.result
+
     };
+
     reader.readAsText(files[0]);
-    console.log(reader);
+
 }
